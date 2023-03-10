f6539abb60159ba585ec787dcb442dc46fb324c5
nitishjaat <72074966+nitishjaat@users.noreply.github.com>
2023-03-10 15:23:53 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -62,7 +62,7 @@ async def give_filter(client, message):
 async def next_page(bot, query):
     ident, req, key, offset = query.data.split("_")
     if int(req) not in [query.from_user.id, 0]:
-        return await query.answer("oKda", show_alert=True)
+        return await query.answer("Request your Own movie", show_alert=True)
     try:
         offset = int(offset)
     except:
