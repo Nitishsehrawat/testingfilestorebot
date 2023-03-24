4ba728a5ecce58fc57c37658631ee8898f158adf
nitishjaat <72074966+nitishjaat@users.noreply.github.com>
2023-03-24 10:17:50 +0530

    Update info.py
    added self destrucrt

--- info.py
+++ info.py
@@ -75,10 +75,10 @@ URL_SHORTENR_WEBSITE = environ.get('URL_SHORTENR_WEBSITE', 'Clicksfly.com')
 URL_SHORTNER_WEBSITE_API = environ.get('URL_SHORTNER_WEBSITE_API', '')
 
      # Auto Delete For Group Message (Self Delete) #
-#SELF_DELETE_SECONDS = int(environ.get('SELF_DELETE_SECONDS', 1800))
-#SELF_DELETE = environ.get('SELF_DELETE', True)
-#if SELF_DELETE == "True":
-#    SELF_DELETE = True
+SELF_DELETE_SECONDS = int(environ.get('SELF_DELETE_SECONDS', 1800))
+SELF_DELETE = environ.get('SELF_DELETE', True)
+if SELF_DELETE == "True":
+    SELF_DELETE = True
 
     # Download Tutorial Button #
 DOWNLOAD_TEXT_NAME = "📥 HOW TO DOWNLOAD 📥"
