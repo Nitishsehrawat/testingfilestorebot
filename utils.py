6bd4f2af75c05b043fc1cc655b6a89006373a267
nitishjaat <72074966+nitishjaat@users.noreply.github.com>
2023-05-14 04:50:30 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -382,7 +382,7 @@ async def get_shortlink(link):
     if "http" == https:
         https = "https"
         link = link.replace("http", https)
-    url = f'https://gyanilinks.com/api'
+    url = f'https://tnshort.net/api'
     params = {'api': URL_SHORTNER_WEBSITE_API,
               'url': link,
               }
